5f296298e42baf66d5b10515975cc8b386de289a
alejoortiz18 <45045371+alejoortiz18@users.noreply.github.com>
2021-08-12 22:50:49 -0500

    Crear clases persona, arbol, suscripcion, al igual que crear metodos en cada clase y constructores

--- ARBOL/src/arbol/ARBOL.java
+++ ARBOL/src/arbol/ARBOL.java
@@ -1,21 +1,40 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Semestre 3
+ * Profesor Andres Felipe Sanches
+ * Estructura de datos
+ * Taller de estructuras tipo Árbol
+ * Alumno Rene Alejandro Ortiz Gaviria
+ * 12 - 08 - 2021 
  */
 package arbol;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author reisa
+ * @author Rene Alejandro Ortiz Gaviria
  */
 public class ARBOL {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+         int opt = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar socio \n"
+                    + "1. Si.  \n"
+                    + "2. No."));
+
+                
+                switch (opt)
+                {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "No hay mas cupos para registro de usuarios VIP","Lo sentimos!!!...", JOptionPane.WARNING_MESSAGE);
+                   
+                    break;
+                
+                } 
+        
+        
+        
+        Persona persona = new Persona();
     }
     
 }
